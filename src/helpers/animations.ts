8449177dfe3e8102385e8fa54998d8a349880f49
Suneeth <suneeth.since2005@gmail.com>
2023-04-02 20:19:54 +0530

    Configured framer motions variant

---
+++ src/helpers/animations.ts
@@ -0,0 +1,31 @@
+export const navbarVariant = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+}
+
+export const navbarItemVariant = {
+  hidden: {
+    opacity: 0,
+    y: 5
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+  }
+}
+
+export const navbarSocialItemVarient = {
+  hidden: {
+    opacity: 0,
+    y: 5
+  },
+  show: {
+    opacity: 0.6,
+    y: 0,
+  }
+}
